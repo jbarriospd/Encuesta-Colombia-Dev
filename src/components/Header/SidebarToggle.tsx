95af608a21aba6d80730e00576e5ed12c49bfeb7
Jose Barrios <jose.barrios@pi.edu.co>
2023-01-21 21:35:14 -0500

    fix: navbar mobile

--- src/components/Header/SidebarToggle.tsx
+++ src/components/Header/SidebarToggle.tsx
@@ -25,9 +25,8 @@ const MenuToggle: FunctionalComponent = () => {
 				xmlns="http://www.w3.org/2000/svg"
 				width="1em"
 				height="1em"
-				fill="none"
 				viewBox="0 0 24 24"
-				stroke="currentColor"
+				class="stroke-white"
 			>
 				<path
 					stroke-linecap="round"
